050e9e4bd9949e862400543d027f1c17ca080bcd
Jareth Moraga <111523122+NotJarethGG@users.noreply.github.com>
2023-11-06 13:05:36 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -43,25 +43,24 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'cgykhe.stackhero-network.com'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'NDP1sMgHDj5WdfTjSYgo3U006Mvt4FSW'),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+                'mysql' => [
+              'driver' => 'mysql',
+              'host' => env('cgykhe.stackhero-network.com'),
+              'port' => 3306,
+              'username' => env('root'),
+              'password' => env('NDP1sMgHDj5WdfTjSYgo3U006Mvt4FSW'),
+              'database' => env('forge'),
+              'charset' => 'utf8mb4',
+              'collation' => 'utf8mb4_unicode_ci',
+              'prefix' => '',
+              'prefix_indexes' => true,
+              'strict' => true,
+              'engine' => null,
+              'sslmode' => 'require',
+              'options' => extension_loaded('pdo_mysql') ? array_filter([
+                  PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+              ]) : [],
+            ],
 
         
 
